d2ab01c921bd02e42679aa9fec18c2ee0f05b7cb
dr_njitram <martijn.gobes@gmail.com>
2021-12-01 13:24:36 +0100

    Day 1 slight improvement

--- day1.erl
+++ day1.erl
@@ -4,32 +4,20 @@
 
 
 part1(Lines) ->
-    check_heights(["999"] ++ [ list_to_integer(Line) || Line <- Lines], 0).
+    check_heights(["999"] ++ [list_to_integer(Line) || Line <- Lines], 0).
 
 
-check_heights([_], Acc) ->
-    Acc;
-check_heights(Lines, Acc) ->
-    [H1, H2| Rest] = Lines,
-    check_heights(
-        [H2] ++ Rest, 
-        Acc + if H1 < H2 -> 1;
-            true -> 0
-        end
-    ).
+check_heights([_], Acc) -> Acc;
+check_heights([H1, H2 | T], Acc) when H1 < H2 ->
+    check_heights([H2|T], Acc + 1);
+check_heights([_|T], Acc) ->
+    check_heights(T, Acc).
 
-check_heights_window([_, _ ,_], Acc) ->
-    Acc;
-check_heights_window(Lines, Acc) ->
-    [H1, _, _, H4| _] = Lines,
-    [_|Tail] = Lines,
-    check_heights_window(
-        Tail, 
-        Acc + if H1 < H4 -> 1;
-            true -> 0
-        end
-    ).
+check_heights_window([_, _, _], Acc) -> Acc;
+check_heights_window([H1, H2, H3, H4 | T], Acc) when H1 < H4 ->
+    check_heights_window([H2, H3, H4|T], Acc + 1);
+check_heights_window([_|T], Acc) ->
+    check_heights_window(T, Acc).
 
 part2(Lines) ->
     check_heights_window([list_to_integer(Line) || Line <- Lines], 0).
-
