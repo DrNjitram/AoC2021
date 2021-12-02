eafeb605b76fb0e6398ddd391ad6a920858610e9
dr_njitram <martijn.gobes@gmail.com>
2021-12-02 22:56:02 +0100

    its much better/worse now

--- day2.erl
+++ day2.erl
@@ -2,35 +2,36 @@
 
 -export([part1/1, part2/1]).
 
+tokenizer(Lines) ->
+    [{A, list_to_integer(B)} || [A, B] <- [ string:tokens(Line, " ") || Line <- Lines ]].
+
 get_depth_height([], Depth, Distance) ->
     Depth * Distance;
-get_depth_height([Command|Rest], Depth, Distance) ->
-    [Direction, Value] = string:tokens(Command, " "),
+get_depth_height([{Direction, Value}|Rest], Depth, Distance) ->
     case Direction of
         "forward" -> 
-            get_depth_height(Rest, Depth, Distance + list_to_integer(Value));
+            get_depth_height(Rest, Depth, Distance + Value);
         "down" ->
-            get_depth_height(Rest, Depth + list_to_integer(Value), Distance);
+            get_depth_height(Rest, Depth + Value, Distance);
         "up" ->
-            get_depth_height(Rest, Depth - list_to_integer(Value), Distance)
+            get_depth_height(Rest, Depth - Value, Distance)
     end.
 
 get_aim([], Depth, Distance, _) ->
     Depth * Distance;
-get_aim([Command|Rest], Depth, Distance, Aim) ->
-    [Direction, Value] = string:tokens(Command, " "),
+get_aim([{Direction, Value}|Rest], Depth, Distance, Aim) ->
     case Direction of
         "forward" -> 
-            get_aim(Rest, Depth + (Aim * list_to_integer(Value)), Distance + list_to_integer(Value), Aim);
+            get_aim(Rest, Depth + (Aim * Value), Distance + Value, Aim);
         "down" ->
-            get_aim(Rest, Depth, Distance, Aim  + list_to_integer(Value));
+            get_aim(Rest, Depth, Distance, Aim  + Value);
         "up" ->
-            get_aim(Rest, Depth, Distance, Aim  - list_to_integer(Value))
+            get_aim(Rest, Depth, Distance, Aim  - Value)
     end.
 
 
 part1(Lines) ->
-    get_depth_height(Lines, 0, 0).
+    get_depth_height(tokenizer(Lines), 0, 0).
 
 part2(Lines) ->
-    get_aim(Lines, 0, 0, 0).
+    get_aim(tokenizer(Lines), 0, 0, 0).
