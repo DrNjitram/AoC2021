e02690bbd07f055d7b7bbad82ec92f766832db24
dr_njitram <martijn.gobes@gmail.com>
2021-12-03 21:55:48 +0100

    Day 3 Part 2

--- day3.erl
+++ day3.erl
@@ -7,11 +7,16 @@ count(Sub, String) -> erlang:length( binary:split(binary:list_to_bin(String), bi
 convert(Value) ->
     list_to_integer(Value, 2).
 
-get_counts(Line) ->
+get_counts1(Line) ->
+    get_counts(Line, "1", "0").
+get_counts0(Line) ->
+    get_counts(Line, "0", "1").
+
+get_counts(Line, R1, R2) ->
     C1 = count("1", Line),
     C0 = count("0", Line),
-    if C1 > C0 -> "1";
-        true -> "0"
+    if C1 >= C0 -> R1;
+        true -> R2
     end.
 
 get_positional(List) ->
@@ -26,13 +31,26 @@ get_positional(List, Acc) ->
     end.
 
 
+get_subset(L, Positionals, Pos, F) ->
+    Most = F(lists:nth(Pos, Positionals)), 
+    [X || X <- L, [lists:nth(Pos, X)] == Most].
+
+build_common_string([Loser], _ ,_) ->
+    Loser;
+build_common_string(L, Position, F) ->
+    Pos = get_positional(L),
+    SubL = get_subset(L, Pos, Position, F),
+    build_common_string(SubL, Position + 1, F).
+
 part1(Lines) ->
     Positionals = get_positional(Lines),
-	Gamma = convert(lists:flatten(lists:map(fun get_counts/1, Positionals))),
+	Gamma = convert(lists:flatten(lists:map(fun get_counts1/1, Positionals))),
     Epsilon = trunc(math:pow(2, length(hd(Lines)))) - 1 - Gamma,
     Gamma * Epsilon.
 
 part2(Lines) ->
-    ok.
-
-
+    Most = build_common_string(Lines, 1, fun get_counts1/1),
+    Least = build_common_string(Lines, 1, fun get_counts0/1),
+    A = convert(lists:flatten(Most)),
+    B = convert(lists:flatten(Least)),
+    A * B.
