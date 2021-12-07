034cedc51c7f6cab59342a740488ec74097c70c4
dr_njitram <martijn.gobes@gmail.com>
2021-12-07 18:01:18 +0100

    faster

--- day7.erl
+++ day7.erl
@@ -2,35 +2,18 @@
 
 -export([part1/1, part2/1]).
 
-create_costs(Max) ->
-    create_costs(Max + 1, [0], 1).
-create_costs(Max, Acc, Max) -> Acc;
-create_costs(Max, Acc, Curr) ->
-    create_costs(Max, Acc ++ [lists:last(Acc) + Curr], Curr + 1).
+create_costs(Max) -> [round(N*(N+1)/2) || N <- lists:seq(0, Max + 1)].
 
+get_fuel1(Val, Vals) -> lists:sum([abs(Val - V) || V <- Vals]).
 
-get_fuel1(Val, Vals) ->
-    lists:sum([abs(Val - V) || V <- Vals]).
-
-get_fuels(Vals) ->
-    Min = lists:min(Vals),
-    Max = lists:max(Vals),
-    Range =  Max - Min,
-    All_costs = create_costs(Range),
-    get_fuels1(lists:seq(Min, Max), 9999999999999999999999999, Vals, All_costs).
-get_fuels1([], Low, _, _) -> Low;
-get_fuels1([Curr|Rest], Low, L, All_costs) ->
-    NewFuel = lists:sum([lists:nth(abs(Curr - V2) + 1, All_costs) || V2 <- L]),
-    if NewFuel < Low -> get_fuels1(Rest, NewFuel, L, All_costs);
-        true -> get_fuels1(Rest, Low, L, All_costs)
-    end.
+get_fuel2(Val, Vals, Costs) -> lists:sum([ lists:nth(abs(Val - V) + 1, Costs) || V <- Vals]).
 
 
 part1([Lines]) ->
     Vals = [list_to_integer(X) || X <- string:tokens(Lines, ",")],
-    Fuels = [get_fuel1(V, Vals) || V <- lists:seq(lists:min(Vals), lists:max(Vals))],
-    hd(lists:sort(Fuels)).
+    get_fuel1(lists:nth(round(length(Vals)/2),lists:sort(Vals)), Vals).
 
 part2([Lines]) ->
     Vals = [list_to_integer(X) || X <- string:tokens(Lines, ",")],
-    get_fuels(Vals).
+    [Min, Max] = [lists:min(Vals), lists:max(Vals)],
+    get_fuel2(round(lists:sum(Vals)/(length(Vals) + 0.5)), Vals, create_costs(Max - Min)).
