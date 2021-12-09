be2ee2f046c95fc98a514cf8604e317ea896485c
dr_njitram <martijn.gobes@gmail.com>
2021-12-09 19:22:11 +0100

    Stuff

--- day9.erl
+++ day9.erl
@@ -2,6 +2,8 @@
 
 -export([part1/1, part2/1]).
 
+
+
 get_adjecents({X0, Y0}, Map) -> [maps:get({X0 + X, Y0 + Y}, Map, 9) || {X, Y} <- [{0, -1}, {0, 1}, {1, 0}, {-1, 0}]].
 check_low({X, Y}, Map) -> length([Height || Height <- get_adjecents({X, Y}, Map), Height =< maps:get({X, Y}, Map)]) == 0.
 get_low_locations(Map) -> [{X, Y} || {X, Y} <- maps:keys(Map), check_low({X, Y}, Map)].
@@ -13,8 +15,7 @@ get_basin_area([{X, Y}| Rest], Map, Acc, Checked) ->
     MoreBasin = [{X0, Y0} || {{X0, Y0}, Height} <- Adj, lists:member({X0, Y0}, Checked) == false, Height < 9],
     get_basin_area(Rest ++ MoreBasin, Map, Acc + length(MoreBasin), Checked ++ [{X0, Y0} || {{X0, Y0}, _} <- Adj]).
 
-answer(Areas) ->
-    sum_list(lists:sort(fun(A, B) -> A > B end, Areas)).
+answer(Areas) -> sum_list(lists:sort(fun(A, B) -> A > B end, Areas)).
 sum_list([A, B, C|_]) -> A * B * C.
 
 part1(Lines) ->
